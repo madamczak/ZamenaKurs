edee1958d1def980c41ad34be9168ec7f75a4695
adamczak.mateusz@gmail.com <adamczak.mateusz@gmail.com>
2021-01-15 15:26:21 +0100

    zadania dzien 1 + notatki z lekcji

--- dzien1uruchamianie/uruchamianie.py
+++ dzien1uruchamianie/uruchamianie.py
@@ -1,1 +1,24 @@
-print("Hello world")
+#C = 5/9 * (F - 32)
+
+monday_temp = 38
+tuesday_temp = 41
+wednesday_temp = 50
+thursday_temp = 32
+friday_temp = 44
+#ctrl + d
+monday_temp_celsius = 5/9 * (monday_temp - 32)
+tuesday_temp_celsius = 5/9 * (tuesday_temp - 32)
+wednesday_temp_celsius = 5/9 * (wednesday_temp - 32)
+thursday_temp_celsius = 5/9 * (thursday_temp - 32)
+friday_temp_celsius = 5/9 * (friday_temp - 32)
+#ctrl + slasz na Suwałki - komentowanie bloku kodu
+# print(monday_temp_celsius)
+# print(tuesday_temp_celsius)
+# print(wednesday_temp_celsius)
+# print(thursday_temp_celsius)
+# print(friday_temp_celsius)
+
+if thursday_temp_celsius > 5:
+    print("Temp wieksza od 5" + u'\xb0' + "C")
+else:
+    print("Temp mniejsza od 5" + u'\xb0' "C")
---
+++ dzien1uruchamianie/zadaniadzien1.py
@@ -0,0 +1,51 @@
+# Zad 1. - Zmienne
+# Mamy podane temperatury w stopniach F dla 7 dni tygodnia dla Boston, MA, JU ES of EJ:
+# 20, 25, 30, 32, 44, 48, 40
+# dla każdego z dni przelicz na stopnie C i przypisz do zmiennej.
+
+# Mamy podane temperatury w stopniach C dla 7 dni tygodnia - 1, 2, -2, -3, 0, 1, -1 dla Lęborka
+# dla każdego z dni przelicz na stopnie F i przypisz do zmiennej.
+
+# Zad 2. - Warunki
+# Na pewno w poprzednim zadaniu użyłeś porządnych nazw zmiennych, w których każde słowo było od siebie oddzielone _ i
+# wszystkie słowa były z małych liter? Na pewno?
+
+# Wybierz sobie jeden z dni tygodnia z poprzedniego zadania i przeprowadź symulację rozmowy o pogodzie.
+# Jeżeli w Lęborku jest zimniej niż w Bostonie wyprintuj na ekran -
+# "Oooo, Panie, u nas w Lęborgu to zimno, nie to co tam w Bostonie",
+# Jeżeli w Bostonie jest zimniej to wyprintuj na ekran -
+# "Oooo, Panie, u nas w Lęborgu to ciepło, nie to co tam w Bostonie",
+
+# Samodzielnie wymyśl co możesz powiedzieć jeżeli temperatury będą takie same.
+
+# Dodatkowo możesz wyprintować jakie temperatury panują w obu tych miastach
+# (tutaj może być potrzebne castowanie temperatury na typ string np. tak - str(32))
+
+# Zad 3. - Obliczenia
+# Jaka jest średnia temperatura w Lęborku z zadania 1.
+
+# Zad 4. - Listy
+# Mamy listę temperatur dla Bostonu - [20, 25, 30, 32, 44, 48, 40]
+# Python ma wbudowane funkcje do różnych operacji na listach. Znajdź w google i wykonaj następujące operacje:
+# suma wszystkich temperatur
+# maksymalna wartość temperatury
+# minimalna wartość temperatury
+# Posortuj listę rosnąco
+# Posortuj listę malejąco
+# Dodaj do listy wartość 666
+# znajdź index elementu o wartości 44
+# sprawdź co robi metoda pop
+# sprawdź co robi metoda append
+
+
+# Zad 5 - Stringi
+# Mamy fragment loga:
+log_fragment = "2020-12-30T16:32:40 at=info method=POST path="/gettherapistjsondetails/" status=200 bytes=9575 protocol=https"
+# popraw powyższą linię w taki sposób żeby python mógł przypisać ją do zmiennej
+# wywołaj metodę split() na log_fragment - nie wiesz jak? poszukaj w necie
+# otrzymasz listę elementów z tego stringa oddzielonych spacjami
+# przypisz do zmiennej date_and_time odpowiedni element
+# przypisz do zmiennej response_status odpowiedni element
+
+# Do metody split możesz podać dowolny znak lub fragment testu którym chcesz podzielić stringa -
+# utwórz zmienne date, time i status i przypisz im odpowiednie wartości
