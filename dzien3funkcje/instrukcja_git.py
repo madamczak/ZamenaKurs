9ec42ca887a3a6f53a7ccf299e0f04f60ae5f022
adamczak.mateusz@gmail.com <adamczak.mateusz@gmail.com>
2021-02-02 19:25:18 +0100

    add gitignore

---
+++ dzien3funkcje/instrukcja_git.py
@@ -0,0 +1,5 @@
+#Obecnie wszyscy pushują commity do mastera i nikt na siebie nie patrzy, ale może to doprowadzić do konfliktów
+#a tego nie chcemy
+#Będziemy więc działać w następujący sposób
+#Przed rozpoczęciem pracy upewniamy się, że mamy czyste repo odpalając komendę
+# git status
