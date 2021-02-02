1b9a5c7deaf9c1c9a507d50a276ec1af21f5a991
adamczak.mateusz@gmail.com <adamczak.mateusz@gmail.com>
2021-02-02 19:51:53 +0100

    add new instructions

--- dzien3funkcje/instrukcja_git.py
+++ dzien3funkcje/instrukcja_git.py
@@ -3,3 +3,14 @@
 #Będziemy więc działać w następujący sposób
 #Przed rozpoczęciem pracy upewniamy się, że mamy czyste repo odpalając komendę
 # git status
+# dostajemy info że nasz branch to master i że nie ma żadnych plików do scommitowania
+# jak masz jakieś pliki to se je wyczyść albo zrób z nimi co trzeba
+
+#Tworzymy brancha przez komendę:
+#git checkout -b nazwa_brancha
+#argument -b tworzy nam nowego brancha, jeżeli chcemy przejść na brancha już stworzonego to nie uzywamy tej opcji
+
+# pracujemy i commitujemy normalnie jak trzeba na swoim lokalnym branchu
+
+#czas wypchnąć brancha na repo po raz pierwszy:
+#git push -u origin nazwa_brancha
