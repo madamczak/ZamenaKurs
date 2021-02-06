b60f1c1a74e0a778f55800d0f359d3aa6f546613
przemysaw <przemyslaw.liebrecht@gmail.com>
2021-02-06 20:37:32 +0200

    test

---
+++ dzien3funkcje/przemysaw3.py
@@ -0,0 +1,49 @@
+#zadania:
+
+# Liczenie + Daty
+
+#1. napisz funkcję konwertującą temp w F na C, która przyjmuje listę temperatur i zwaraca listę
+# ze skonwetowanymi wartościami
+def fahrentheit_to_c(temp_in_f, precision=2):
+    if type(temp_in_f) == str:
+        print("Źle, popraw argument")
+
+    else:
+        return round(5 / 9 * (temp_in_f - 32), precision)
+
+# Jak sprawdzisz czy ta funkcja działa dobrze?
+# o tak:
+assert fahrentheit_to_c(32, precision=2) == 0
+assert fahrentheit_to_c(212, precision=2) == 100
+
+#2. napisz funkcję która policzy średnią arytmetyczną wartości z listy podanej jako argument do funkcji
+
+def artmtc_avg():
+
+# Jakie tu mogą być problemy? Jak sprawdzisz czy funkcja działa dobrze?
+
+#3. napisz funkcję generującą losowe obiekty datetime. Funkcja posiada argument wejściowy ile_dni, a zwraca listę losowych
+#obiektów datetime o takiej długości jak argument ile_dni.
+# Jak sprawdzisz czy funkcja działa dobrze?
+# Dodaj opcjonalny argument do funkcji który będzie decydował o tym czy zwaracan lista jest posortowana lub nie
+# Jak sprawdzisz działanie dodatkowego argumentu?
+
+# Pętle
+# 4. Łańcuch DNA można przedstawić w postaci stringa zawierającego litery A, C, T i G
+# (od nazw: Adenina, Cytozyna, Tymina i Guanina) np tak:
+# 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+# a) Napisz funkcję adenine_count, która używając pętli for zwróci ile jest nukleotydów A w łańcuchu DNA
+# b) Napisz funkcję adenine_count_while, która używając pętli while zwróci ile jest nukleotydów A w łańcuchu DNA
+# c) Napisz funkcję nucleotide_count, która używając dowolnej pętli zwróci ile jest
+# nukleotydów podanych w argumencie (nucleotide_type) w łańcuchu DNA
+# Jak inaczej można policzyć wystąpienia nukleotydó w łańcuchu? Które podejście jest szybsze? Jak to sprawdzisz?
+# d) Napisz funkcję która jako argument przyjmie łańcuch DNA i zwróci słownik
+# w którym kluczem będzie nukleotyd a wartością ilość jego wystąpień w tym łańcuchu
+
+# e) W pliku rosalind_dna.txt masz łańcuch DNA zawierający około 900 nukleotydów. Wczytaj go i podaj zawartość
+# A, T, C i G w tym łańcuchu.
+
+# 5. Łańcuch RNA jest bardzo podobny do DNA i składa się z następujących nukleotydów A, C, U i G (U - uracyl).
+# Twoim celem jest napisanie takiego samego zestawu funkcji dla RNA jak dla DNA w zadaniu 4. Jak to zrobisz?
+# Będziesz kopiował? Będziesz modyfikował funkcje z zadania 4, tak żeby obsługiwały oba typy łańcuchów?
+# Które podejście będzie lepsze i co z niego wyniknie?
