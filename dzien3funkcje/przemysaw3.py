17eba9c060de9fd108ea6189b4dbbbf9b5eb8fa4
przemysaw <przemyslaw.liebrecht@gmail.com>
2021-02-14 13:11:19 +0200

    przemysaw

--- dzien3funkcje/przemysaw3.py
+++ dzien3funkcje/przemysaw3.py
@@ -4,6 +4,7 @@
 
 #1. napisz funkcję konwertującą temp w F na C, która przyjmuje listę temperatur i zwaraca listę
 # ze skonwetowanymi wartościami
+
 def fahrentheit_to_c(temp_in_f, precision=2):
     if type(temp_in_f) == str:
         print("Źle, popraw argument")
@@ -18,12 +19,40 @@ assert fahrentheit_to_c(212, precision=2) == 100
 
 #2. napisz funkcję która policzy średnią arytmetyczną wartości z listy podanej jako argument do funkcji
 
-def artmtc_avg():
+def artmtc_avg(numbers):
+    return float(sum(numbers)) / max(len(numbers), 1)
+
+lista_sredniej=[1,2,3,4,5]
+
+#print(artmtc_avg(lista_sredniej))
 
 # Jakie tu mogą być problemy? Jak sprawdzisz czy funkcja działa dobrze?
 
+# ja tam nigdy nie widzę problemów niestety, a sprawdzę o tak:
+assert artmtc_avg([1,2,3,4,5]) == 3
+
 #3. napisz funkcję generującą losowe obiekty datetime. Funkcja posiada argument wejściowy ile_dni, a zwraca listę losowych
 #obiektów datetime o takiej długości jak argument ile_dni.
+
+# import random
+# import datetime
+#
+# random_integer_1 = random.randint(1, 31)
+# random_integer_2 = random.randint(1, 12)
+# random_integer_3 = random.randint(1970, 2021)
+#
+# date_from_random = str(random_integer_1) + '-' + str(random_integer_2) + '-' + str(random_integer_3)
+#
+# obiekt_datetime = datetime.datetime.strptime(date_from_random, '%d-%m-%Y')
+# print(obiekt_datetime)
+#
+# def random_datetime(ile_dni):
+#     random_datetime_list=[]
+#     while ile_dni > 0:
+#         ile_dni = ile_dni - 1
+#         random_datetime_list.pop(obiekt_datetime)
+
+
 # Jak sprawdzisz czy funkcja działa dobrze?
 # Dodaj opcjonalny argument do funkcji który będzie decydował o tym czy zwaracan lista jest posortowana lub nie
 # Jak sprawdzisz działanie dodatkowego argumentu?
@@ -32,7 +61,12 @@ def artmtc_avg():
 # 4. Łańcuch DNA można przedstawić w postaci stringa zawierającego litery A, C, T i G
 # (od nazw: Adenina, Cytozyna, Tymina i Guanina) np tak:
 # 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+dna = AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
 # a) Napisz funkcję adenine_count, która używając pętli for zwróci ile jest nukleotydów A w łańcuchu DNA
+
+def adenine_count(dna):
+    while
+    
 # b) Napisz funkcję adenine_count_while, która używając pętli while zwróci ile jest nukleotydów A w łańcuchu DNA
 # c) Napisz funkcję nucleotide_count, która używając dowolnej pętli zwróci ile jest
 # nukleotydów podanych w argumencie (nucleotide_type) w łańcuchu DNA
