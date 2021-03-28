713d1ea5b9e2bf7c7b94841018f236dee4774dff
przemysaw <przemyslaw.liebrecht@gmail.com>
2021-03-28 20:12:46 +0200

    add almost ready function check_if_link_exists

--- scrapper/databases/sqlite_operations.py
+++ scrapper/databases/sqlite_operations.py
@@ -3,38 +3,57 @@ import sqlite3
 from scrapper.trojmiasto.website_parser import linck2dick
 
 
-def create_db_if_not_exists(database_file_name):
-    con = sqlite3.connect(database_file_name)
-    cur = con.cursor()
-
-    cur.execute('''CREATE TABLE IF NOT EXISTS real_estate
-                   (price int, address text, square_meter_price real, building_year int, area real, floor int,
-                    building_type text, heating_type text, number_of_rooms int, building_floors int, link text)''')
-
-    con.close()
+# def create_db_if_not_exists(database_file_name):
+#     con = sqlite3.connect(database_file_name)
+#     cur = con.cursor()
+#
+#     cur.execute('''CREATE TABLE IF NOT EXISTS real_estate
+#                    (price int, address text, square_meter_price real, building_year int, area real, floor int,
+#                     building_type text, heating_type text, number_of_rooms int, building_floors int, link text)''')
+#
+#     con.close()
 
 # P
-def check_if_link_exists(url):
-    pass
-
-
-# D
-def insert_record(database_file_name, flat_dictionary):
+def check_if_link_exists(database_file_name, lnk):
     con = sqlite3.connect(database_file_name)
     cur = con.cursor()
+    cur.execute("SELECT COUNT(*) FROM real_estate WHERE link='https://ogloszenia.trojmiasto.pl/nieruchomosci-rynek-wtorny/mieszkanie-2-pokoje-gdynia-doskonala-inwestycja-ogl64070755.html'")
+    all_values = cur.fetchall()
+    for v in all_values:
+        num_of_occur = int(str(v).replace("(","").replace(",)",""))
 
-    cmd = f"INSERT INTO real_estate VALUES ({flat_dictionary.get('price')}, '{flat_dictionary.get('address')}')"
+        if num_of_occur >= 1:
+            break
 
 
-    cur.execute(cmd)
+    # for a in num_of_occur:
+    #     if a >= 1:
+    #         return "we have this in the db already"
 
-    con.close()
+
+    # if int(str(all_values).removeprefix("(").removesuffix(",)") > 1:
+    #     return "Ad is in the DB already"
+
+check_if_link_exists("example.db", 'https://ogloszenia.trojmiasto.pl/nieruchomosci-rynek-wtorny/mieszkanie-2-pokoje-gdynia-doskonala-inwestycja-ogl64070755.html')
+
+# D
+# def insert_record(database_file_name, flat_dictionary):
+#     con = sqlite3.connect(database_file_name)
+#     cur = con.cursor()
+#
+#     cmd = f"INSERT INTO real_estate VALUES ({flat_dictionary.get('price')}, '{flat_dictionary.get('address')}, '{flat_dictionary.get('square_meter_price')}, '{flat_dictionary.get('building_year')}, '{flat_dictionary.get('area')}, '{flat_dictionary.get('floor')}, '{flat_dictionary.get('building_type')}, '{flat_dictionary.get('heating_type')}, '{flat_dictionary.get('number_of_rooms')}, '{flat_dictionary.get('building_floors')}, '{flat_dictionary.get('link')}')"
+#
+#
+#
+#     cur.execute(cmd)
+#
+#     con.close()
 
 # create_db_if_not_exists('example.db')
 
 dct = linck2dick("https://ogloszenia.trojmiasto.pl/nieruchomosci-rynek-wtorny/mieszkanie-2-pokoje-gdynia-doskonala-inwestycja-ogl64070755.html")
-
-insert_record("example.db", dct)
+#
+# insert_record("example.db", dct)
 
 
 # cur.execute("INSERT INTO stocks VALUES ('2006-01-05','SELL','RHAT',111,35.14)")
